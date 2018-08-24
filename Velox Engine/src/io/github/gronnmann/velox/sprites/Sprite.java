cdfd390be46bb7d565e0c55ddde92fb201a1f0f9
gronnmann <gronnmann@users.noreply.github.com>
2018-08-24 20:23:45 +0200

    Sprite update :)

--- Velox Engine/src/io/github/gronnmann/velox/sprites/Sprite.java
+++ Velox Engine/src/io/github/gronnmann/velox/sprites/Sprite.java
@@ -15,6 +15,8 @@ public class Sprite {
 	protected Image img;
 	private String imgN;
 	
+	private boolean invisible = false;
+	
 	protected boolean deadly = false;
 	
 	public Sprite(int x, int y, int width, int height, String img){
@@ -24,12 +26,32 @@ public class Sprite {
 		this.height = height;
 		Print.debug("Sprite: Fetching image from location: /" + img + ".png");
 		Print.debug("Bounds: " + x + ", " + y + ", " + width + ", " + height);
-		this.img = new ImageIcon(getClass().getResource("/" + img + ".png")).getImage();
-		this.img = this.img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		
+		
+		try{
+			this.img = new ImageIcon(getClass().getResource("/" + img + ".png")).getImage();
+			this.img = this.img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		}catch(Exception e){
+			Print.error("Sprite: " + img + " not found. Setting invisible.");
+			invisible = true;
+		}
+		
+		
 		this.imgN = img;
 	}
 	
+	public Sprite(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		
+		Print.debug("Creating invisible sprite");
+		invisible = true;
+	}
+	
 	public void rescale() {
+		if (invisible)return;
 		this.img = new ImageIcon(getClass().getResource("/" + img + ".png")).getImage();
 		this.img = this.img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
 		Print.debug("Rescaling sprite. New bounds: " + x + ", " + y + ", " + width + ", " + height);
@@ -50,6 +72,13 @@ public class Sprite {
 		return y;
 	}
 	
+	public int getWidth(){
+		return width;
+	}
+	public int getHeight(){
+		return height;
+	}
+	
 	public void update(){
 		processMovement();
 	}
@@ -59,13 +88,24 @@ public class Sprite {
 		y-=moveY;
 	}
 	
-	public void deprocessMovement() {
-		x-=moveX;
-		y+=moveY;
+	public void deprocessMovement(boolean x, boolean y) {
+		if (x){
+			this.x-=moveX;
+		}
+		if (y){
+			this.y+=moveY;
+		}
 	}
 	
 	public boolean isDeadly() {
 		return deadly;
+	}
+	
+	public boolean isVisible(){
+		return !invisible;
+	}
+	public void setVisible(boolean visible){
+		this.invisible = !visible;
 	}
 	
 	public boolean collidesWithAnother(ArrayList<Sprite> sprites){
@@ -77,12 +117,13 @@ public class Sprite {
 		return false;
 	}
 	
-	public Sprite getCollided(ArrayList<Sprite> sprites){
+	public ArrayList<Sprite> getCollided(ArrayList<Sprite> sprites){
+		ArrayList<Sprite> coll = new ArrayList<>();
 		for (Sprite s : sprites){
 			if (s != this){
-				if (s.getBounds().intersects(this.getBounds()))return s;
+				if (s.getBounds().intersects(this.getBounds()))coll.add(s);
 			}
 		}
-		return null;
+		return coll;
 	}
 }
